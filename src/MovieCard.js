882463ce4a2d16045651c72f7a4a8787ed29a02f
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-06-02 15:15:26 +0530

    Modofied Changes

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,71 +2,10 @@ import {Component} from "react";
 import './index.css';
 
 class MovieCard extends Component{
-    constructor(){
-        super();
-        this.state = {
-            title:"The Avengers",
-            plot:"Super Natural power shown in the movie.",
-            price:199,
-            rating:8.9,
-            stars: 0,
-            fav: true,
-            cart:true
-        }
-    }
-
-    addStars= () => {
-
-        //F0rm1
-        // this.setState({
-        //     stars : this.state.stars += 0.5
-        // });
-
-        //Form2
-        if (this.state.stars>=5){
-            return;
-        }
-        this.setState((prevState) => {
-            return{
-                stars:prevState.stars+0.5
-            }
-        });
-
-
-        // this.state.stars +=0.5;
-        // console.log("Star is added!",this.state.stars);
-
-    }
-
-    removeStars= () => {
-        if (this.state.stars<=0){
-            return;
-        }
-        // this.setState({
-        //     stars : this.state.stars -=0.5
-        // });
-        this.setState((nxtState) => {
-            return{
-                stars:nxtState.stars-0.5
-            }
-        });
-    }
-
-
-    handleFav = () => {
-        this.setState({
-            fav : !this.state.fav
-        })
-    }
-
-    handleCart = () => {
-        this.setState({
-            cart : !this.state.cart
-        })
-    }
 
     render(){
-        const {title,plot,price,rating,stars,fav,cart} = this.state;
+        console.log(this.props);
+        const {title,plot,price,rating,stars,fav,cart} = this.props.movies;
         return( 
             <div className="main">
                 <div className="movie-card">
@@ -81,21 +20,21 @@ class MovieCard extends Component{
                         <div className="footer">
                             <div className="rating">{rating}</div>
                             <div className="star-dis">
-                            <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/10308/10308996.png" alt="decrease" onClick={this.removeStars}/>
+                            <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/10308/10308996.png" alt="decrease" onClick={() => {this.props.removeStars(this.props.movies)}}/>
                                 <img src="https://cdn-icons-png.flaticon.com/128/477/477406.png" alt="star" className="stars" />
-                            <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/399/399271.png" alt="increase" onClick={this.addStars} />
+                            <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/399/399271.png" alt="increase" onClick={() => {this.props.addStars(this.props.movies)}}/>
                             <span className="starCount">{stars}</span>
                             </div>
 
                             {/* {fav ? <button className="unfavourite-btn" onClick={this.handleFav}>Unfavourite</button>:
                             <button className="favourite-btn" onClick={this.handleFav}>Favourite</button>} */}
 
-                            <button className={fav?"unfavourite-btn":"favourite-btn"} onClick={this.handleFav}>{fav?"Unfavourite":"Favourite"}</button>
+                            <button className={fav?"unfavourite-btn":"favourite-btn"} onClick={() => {this.props.favorite(this.props.movies)}}>{fav?"Unfavourite":"Favourite"}</button>
                             
                            
 
 
-                            <button className={cart?"uncart-btn":"cart-btn"} onClick={this.handleCart}>{cart?"Add to Cart":"Remove from cart"}</button>
+                            <button className={cart?"uncart-btn":"cart-btn"} onClick={() => {this.props.addToCart(this.props.movies)}}>{cart?"Add to Cart":"Remove from cart"}</button>
                         </div>
                     </div>
                 </div>
--- src/MovieList.js
+++ src/MovieList.js
@@ -3,11 +3,165 @@ import './index.css';
 import MovieCard from "./MovieCard";
 
 class MovieList extends Component{
+    constructor(){
+        super();
+        this.state = {
+            movies : [
+                {
+                    title: 'The Avengers',                  
+                    plot:
+                      "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+                    poster:
+                      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+                    rating: '8.0',            
+                    price: 99,            
+                    stars: 0,
+                    fav: false,
+                    cart: false                  
+                  },
+                  {
+                    title: 'The Dark Knight',                  
+                    plot:
+                      'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+                    poster:
+                      'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',                  
+                    rating: '9.0',            
+                    price: 199,            
+                    stars: 0,            
+                    fav: false,
+                    cart: false                  
+                  },
+                  {
+                    title: 'Iron Man',                 
+                    plot:
+                      'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',                  
+                    poster:
+                      'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg',                  
+                    rating: '7.9',            
+                    price: 139,            
+                    stars: 0,            
+                    fav: false,
+                    cart: false                  
+                  }
+            ]
+        }
+    }
+
+    // addStars= () => {
+
+    //     //F0rm1
+    //     // this.setState({
+    //     //     stars : this.state.stars += 0.5
+    //     // });
+
+    //     //Form2
+    //     if (this.state.stars>=5){
+    //         return;
+    //     }
+    //     this.setState((prevState) => {
+    //         return{
+    //             stars:prevState.stars+0.5
+    //         }
+    //     });
+
+
+    //     // this.state.stars +=0.5;
+    //     // console.log("Star is added!",this.state.stars);
+
+    // }
+
+    // removeStars= () => {
+    //     if (this.state.stars<=0){
+    //         return;
+    //     }
+    //     // this.setState({
+    //     //     stars : this.state.stars -=0.5
+    //     // });
+    //     this.setState((nxtState) => {
+    //         return{
+    //             stars:nxtState.stars-0.5
+    //         }
+    //     });
+    // }
+
+
+   
+
+    handleIncStar = (movie) => {
+
+        const {movies} = this.state;
+
+        const mid = this.state.movies.indexOf(movie);
+        if(movies[mid].stars >= 5){
+            return;
+        }
+
+        movies[mid].stars += 0.5;
+
+        this.setState({
+            movies : movies
+
+        })
+    }
+
+    handleDecStar = (movie) => {
+        const {movies} = this.state;
+
+        const mid = this.state.movies.indexOf(movie);
+        if(movies[mid].stars <= 0){
+            return;
+        }
+
+        movies[mid].stars -= 0.5;
+
+        this.setState({
+            movies : movies
+
+        })
+    }
+
+    // handleFav = (movie) => {
+    //     this.setState({
+    //         fav : !this.state.fav
+    //     })
+    // }
+
+    // handleCart = () => {
+    //     this.setState({
+    //         cart : !this.state.cart
+    //     })
+    // }
+
+    handleToggleFav = (movie)=> {
+        const {movies} = this.state;
+        const movieId = movies.indexOf(movie);
+    
+        movies[movieId].fav = !movies[movieId].fav;
+    
+        this.setState({
+            movies
+        })
+      }
+    
+      handleAddToCart = (movie)=> {
+        const {movies} = this.state;
+        const movieId = movies.indexOf(movie);
+    
+        movies[movieId].cart = !movies[movieId].cart;
+    
+        this.setState({
+            movies
+        })
+      }
+    
+
     render(){
-       
+        const {movies}= this.state;
+        // const {title, plot, price, rating, poster, stars, fav, cart} = this.state.movies;
         return(  
             <>
-            <MovieCard />
+            {movies.map((movie) => <MovieCard movies = {movie} addStars= {this.handleIncStar} removeStars= {this.handleDecStar} favorite={this.handleToggleFav} addToCart={this.handleAddToCart} />)}
+            
             </>
         )
     }
